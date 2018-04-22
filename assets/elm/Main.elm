ee622f6bd00f4e5a0be7474d05bb5f206d67361e
NathanYukai <zlcghoul@hotmail.com>
2018-04-22 22:15:09 +0100

    using elm to render

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,8 +1,58 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (Html, input, text)
+import Html.Attributes exposing (..)
 
 
-main : Html a
+type Msg
+    = UpdateUserName String
+
+
+type alias Model =
+    { history : List String
+    , userName : String
+    , message : String
+    }
+
+
+view : Model -> Html Msg
+view model =
+    Html.div []
+        [ Html.ul [] []
+        , Html.div []
+            [ input [ placeholder "Your Name" ] []
+            , input [] []
+            ]
+        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        UpdateUserName name ->
+            { model | userName = name } ! []
+
+
+main : Program Never Model Msg
 main =
-    text "Hello, World sklfjdsklfjdskl!"
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { userName = ""
+      , history = []
+      , message = ""
+      }
+    , Cmd.none
+    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch []
